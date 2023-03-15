ed5507c867b867491c0022e88d8eb23001eb74d9
parselledev <parselledev@gmail.com>
2023-03-15 22:07:57 +0200

    fix server

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://146.185.235.115:3000',
+  'http://146.185.235.115:3500',
 ];
 module.exports = allowedOrigins;
