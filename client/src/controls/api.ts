051dea0df44495023de49b9f462564e064c7104f
parselledev <parselledev@gmail.com>
2023-04-10 22:18:24 +0200

    фиксы под новый апи

--- client/src/controls/api.ts
+++ client/src/controls/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "http://146.185.235.115:3500";
-const baseURL = "http://localhost:3500";
+const baseURL = "http://146.185.235.115:3500";
+// const baseURL = "http://localhost:3500";
 
 const getTracks = () =>
   axios.get("/dozor/tracks", { baseURL }).then((res) => res.data);
