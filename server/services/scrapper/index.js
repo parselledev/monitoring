88f460752934a21008ac3b7e442c08cbf0585a70
parselledev <parselledev@gmail.com>
2023-04-15 13:54:38 +0200

    фиксы с новым интервалом

--- server/services/scrapper/index.js
+++ server/services/scrapper/index.js
@@ -151,14 +151,19 @@ module.exports = async () => {
   });
 
   /** Ожидание кнопки для выхода из сна */
+  // setInterval(async () => {
+  //   await page.waitForSelector('.forms__button_warning', {
+  //     visible: true,
+  //     timeout: 0,
+  //   });
+  //   await page.$eval(
+  //     '.forms__button_warning',
+  //     async (elem) => await elem.click()
+  //   );
+  // }, 1000 * 60 * 2); // 2 мин
+
+  /** Интервальная перезагрузка страницы */
   setInterval(async () => {
-    await page.waitForSelector('.forms__button_warning', {
-      visible: true,
-      timeout: 0,
-    });
-    await page.$eval(
-      '.forms__button_warning',
-      async (elem) => await elem.click()
-    );
-  }, 1000 * 60 * 2);
+    await page.reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
+  }, 1000 * 60 * 2); // 2 мин
 };
