67ab3df24b138094f28a6d0a56ddc21dcde6022b
parselledev <parselledev@gmail.com>
2023-04-15 13:58:38 +0200

    фиксы с новым интервалом

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,11 @@
 const allowedOrigins = [
   'http://localhost:3000',
+  'http://localhost:3500',
+
   'http://127.0.0.1:3000',
-  'http://146.185.235.115:3000',
-  'http://146.185.235.115:3500',
+  'http://127.0.0.1:3500',
+
+  'http://194.58.121.20:3000',
+  'http://194.58.121.20:3500',
 ];
 module.exports = allowedOrigins;
