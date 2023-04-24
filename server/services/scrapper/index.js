b67fb50c8d445fcf371db2857f85906caa4fa0b4
parselledev <parselledev@gmail.com>
2023-04-24 14:21:55 +0200

    fix: апрельский патч - правки

--- server/services/scrapper/index.js
+++ server/services/scrapper/index.js
@@ -120,7 +120,7 @@ module.exports = async () => {
             typeof v === 'object' ? JSON.parse(JSON.stringify(v, null, 2)) : v
           )[1];
 
-          if (signal.ignition !== null) {
+          if (Boolean(signal.ignition)) {
             if (!lodashIsEqual(deviceState, signal)) {
               deviceState = signal;
               await signalModel.create({ ...deviceState });
