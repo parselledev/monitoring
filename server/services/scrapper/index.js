fe6d4a5ebaace4d4a615b3ff210d7a32a9165d29
parselledev <parselledev@gmail.com>
2023-04-26 02:09:12 +0200

    feat: перенос на фронт

--- server/services/scrapper/index.js
+++ server/services/scrapper/index.js
@@ -122,7 +122,9 @@ module.exports = async () => {
 
           if (typeof signal.ignition === 'boolean') {
             if (!lodashIsEqual(deviceState, signal)) {
-              if (signal.guard !== 'true' && deviceState.guard !== 'true') {
+              if (signal.guard === 'true' && deviceState.guard === 'true') {
+                return null;
+              } else {
                 deviceState = signal;
                 await signalModel.create({ ...deviceState });
                 await deviceStateModel.findOneAndUpdate(deviceState);
