6d64784e3fa005b5caae7dd38ac03c43000e7d35
parselledev <parselledev@gmail.com>
2023-05-12 10:46:58 +0200

    feat: передл с перезагрузкой

--- server/services/scrapper/index.js
+++ server/services/scrapper/index.js
@@ -19,7 +19,7 @@ module.exports = async () => {
       if (!connected) {
         window.dozor.run(window.dozor._session);
       }
-    }, 1000 * 10); // 10 сек.
+    }, 1000 * 60); // 1 мин.
 
     setInterval(() => {
       const device = window.dozor._dozor._garage._devices.get(61739);
@@ -39,6 +39,36 @@ module.exports = async () => {
       });
     }, 1000);
   `;
+
+  setInterval(() => {
+    const connected =
+      window.dozor._dozor._garage._devices.get(61739)?._states?.connected ||
+      false;
+
+    if (!connected) {
+      window.dozor.run(window.dozor._session);
+    }
+  }, 1000 * 60); // 1 мин.
+
+  setInterval(() => {
+    const device = window.dozor._dozor._garage._devices.get(61739) || null;
+    const states = device?._states || null;
+    const geo = device?._map?._markers.get(61739)?._geo?.coords || null;
+
+    if (states) {
+      console.log('DOZOR', {
+        geo: geo,
+        guard: states.guard,
+        ignition: states.ignition,
+        driver_door: states.door_fl,
+        front_pass_door: states.door_fr,
+        rear_left_door: states.door_rl,
+        rear_right_door: states.door_rr,
+        trunk: states.trunk,
+        hood: states.hood,
+      });
+    }
+  }, 1000);
 
   let deviceStateRemote = await deviceStateModel.findOne().lean();
 
@@ -137,17 +167,17 @@ module.exports = async () => {
           typeof v === 'object' ? JSON.parse(JSON.stringify(v, null, 2)) : v
         )[1];
 
-        if (typeof signal.ignition === 'boolean') {
-          if (!lodashIsEqual(deviceState, signal)) {
-            if (signal.guard === 'true' && deviceState.guard === 'true') {
-              return null;
-            } else {
-              deviceState = signal;
-              await signalModel.create({ ...deviceState });
-              await deviceStateModel.findOneAndUpdate(deviceState);
-            }
+        // if (typeof signal.ignition === 'boolean') {
+        if (!lodashIsEqual(deviceState, signal)) {
+          if (signal.guard === 'true' && deviceState.guard === 'true') {
+            return null;
+          } else {
+            deviceState = signal;
+            await signalModel.create({ ...deviceState });
+            await deviceStateModel.findOneAndUpdate(deviceState);
           }
         }
+        // }
       }
     } catch (e) {}
   });
